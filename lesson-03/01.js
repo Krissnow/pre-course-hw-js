0c32f8e5065b7f78f1d9a19593beee78bee6aa78
Krissnow <136382532+Krissnow@users.noreply.github.com>
2023-11-26 17:41:41 +0100

    filter join

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,14 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+  let flagArr = [];
+  colors.filter((item) => {
+    if (item === "черный" || item === "красный" || item === "желтый") {
+      flagArr.push(item);
+    }
+  });
+
+  return flagArr.join("-");
+}
+
+console.log(createColorString());
